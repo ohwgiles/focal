d4adbdaa67885bea5c9d44585b1ca027fe322287
Oliver Giles <ohw.giles@gmail.com>
2018-03-24 16:41:00 +0300

    resolves #4: view event details
    Initial support for displaying information about
    the currently selected event

--- event-panel.c
+++ event-panel.c
@@ -13,6 +13,43 @@
  */
 #include "event-panel.h"
 
+typedef struct {
+	GtkWidget* layout;
+	int width;
+	int cheight; // child element height
+	int cx, cy;  // coordinates of current child label
+} AttendeeLayout;
+
+static void attendee_layout_clear(AttendeeLayout* al)
+{
+	gtk_container_foreach(GTK_CONTAINER(al->layout), (GtkCallback) gtk_widget_destroy, NULL);
+}
+
+static void attendee_layout_add(AttendeeLayout* al, const char* name)
+{
+	gtk_container_add(GTK_CONTAINER(al->layout), gtk_label_new(name));
+}
+
+static void attendee_layout_relayout_child(GtkWidget* child, AttendeeLayout* al)
+{
+	int cwidth;
+	gtk_widget_get_preferred_width_for_height(child, al->cheight, NULL, &cwidth);
+	if (al->cx + cwidth > al->width) {
+		al->cy += al->cheight;
+		al->cx = 0;
+	}
+	gtk_layout_move(GTK_LAYOUT(al->layout), child, al->cx, al->cy);
+	al->cx += cwidth;
+}
+
+static void attendee_layout_relayout(GtkWidget* layout, GdkRectangle* allocation, AttendeeLayout* al)
+{
+	al->cx = 0;
+	al->cy = 0;
+	al->width = allocation->width;
+	gtk_container_foreach(GTK_CONTAINER(layout), (GtkCallback) attendee_layout_relayout_child, al);
+}
+
 struct _EventPanel {
 	GtkBox parent;
 	GtkWidget* bar;
@@ -24,8 +61,10 @@ struct _EventPanel {
 	GtkWidget* cancel_button;
 
 	GtkWidget* details;
-	GtkWidget* description;
-	GtkWidget* invited;
+	GtkWidget* starts_at;
+	GtkWidget* ends_at;
+	GtkTextBuffer* description;
+	AttendeeLayout attendees;
 
 	icalcomponent* event;
 };
@@ -36,7 +75,7 @@ enum {
 	LAST_SIGNAL
 };
 
-static gint event_panel_signals[LAST_SIGNAL] = {0};
+static guint event_panel_signals[LAST_SIGNAL] = {0};
 
 static void event_panel_class_init(EventPanelClass* klass)
 {
@@ -45,7 +84,6 @@ static void event_panel_class_init(EventPanelClass* klass)
 
 static void event_panel_init(EventPanel* self)
 {
-	gtk_widget_set_halign((GtkWidget*) self, GTK_ALIGN_FILL);
 	gtk_widget_set_valign((GtkWidget*) self, GTK_ALIGN_END);
 }
 
@@ -55,9 +93,9 @@ static void show_details(GtkToggleButton* togglebutton, gpointer user_data)
 	gtk_widget_set_visible(e->details, gtk_toggle_button_get_active(togglebutton));
 }
 
-static void first_show(GtkWidget* widget, gpointer user_data)
+static void first_show(GtkWidget* widget)
 {
-	EventPanel* ew = FOCAL_EVENT_PANEL(user_data);
+	EventPanel* ew = FOCAL_EVENT_PANEL(widget);
 	gtk_widget_hide(ew->details);
 	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(ew->details_button), FALSE);
 	gtk_widget_hide(ew->bar);
@@ -67,6 +105,14 @@ static void delete_clicked(GtkButton* button, gpointer user_data)
 {
 	EventPanel* ew = FOCAL_EVENT_PANEL(user_data);
 	g_signal_emit(ew, event_panel_signals[SIGNAL_EVENT_DELETE], 0, ew->event);
+}
+
+static inline GtkWidget* field_label_new(const char* label)
+{
+	GtkWidget* lbl = gtk_label_new(label);
+	gtk_widget_set_halign(lbl, GTK_ALIGN_START);
+	gtk_label_set_use_markup(GTK_LABEL(lbl), TRUE);
+	return lbl;
 }
 
 GtkWidget* event_panel_new()
@@ -91,48 +137,79 @@ GtkWidget* event_panel_new()
 	g_signal_connect(e->delete_button, "clicked", (GCallback) &delete_clicked, e);
 	gtk_action_bar_pack_end(GTK_ACTION_BAR(e->bar), e->edit_button);
 
-	e->details = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	GtkWidget* scroll = gtk_scrolled_window_new(0, 0);
-	// TODO
-	gtk_scrolled_window_set_min_content_width(GTK_SCROLLED_WINDOW(scroll), 400);
-	gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(scroll), 400);
-	GtkWidget* left = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	e->description = gtk_label_new("");
-	gtk_label_set_line_wrap(GTK_LABEL(e->description), TRUE);
-	gtk_container_add(GTK_CONTAINER(scroll), e->description);
-	gtk_box_pack_start(GTK_BOX(left), scroll, 0, 0, 15);
-	//    gtk_box_pack_start(GTK_BOX(h), fr, 0, 0, 0);
-	//gtk_action_bar_set_center_widget(GTK_ACTION_BAR(e->details), h);
+	e->details = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
-	GtkWidget* right = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	e->invited = gtk_flow_box_new();
+	GtkWidget* grid_left = gtk_grid_new();
+	gtk_grid_set_column_spacing(GTK_GRID(grid_left), 5);
+	gtk_grid_set_row_spacing(GTK_GRID(grid_left), 5);
+	gtk_grid_attach(GTK_GRID(grid_left), field_label_new("<b>Starts at:</b>"), 0, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid_left), field_label_new("<b>Ends at:</b>"), 0, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid_left), field_label_new("<b>Description</b>"), 0, 2, 2, 1);
 
-	gtk_box_pack_start(GTK_BOX(right), e->invited, 0, 0, 15);
+	e->starts_at = gtk_label_new("");
+	gtk_widget_set_halign(e->starts_at, GTK_ALIGN_END);
+	e->ends_at = gtk_label_new("");
+	gtk_widget_set_halign(e->ends_at, GTK_ALIGN_END);
 
-	//gtk_box_pack_start(GTK_BOX(e->details), left, 0, 0, 15);
-	gtk_box_pack_start(GTK_BOX(e->details), right, 0, 0, 15);
+	GtkWidget* description_scrolled = gtk_scrolled_window_new(0, 0);
+	// TODO: infer appropriate height somehow
+	gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(description_scrolled), 100);
+	GtkWidget* description_view = gtk_text_view_new();
+	gtk_widget_set_hexpand(description_view, TRUE);
+	gtk_container_add(GTK_CONTAINER(description_scrolled), description_view);
+	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(description_view), GTK_WRAP_WORD);
+	e->description = gtk_text_view_get_buffer(GTK_TEXT_VIEW(description_view));
+	gtk_text_buffer_set_text(e->description, "Hello, this is some text", -1);
+	gtk_grid_attach(GTK_GRID(grid_left), e->starts_at, 1, 0, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid_left), e->ends_at, 1, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid_left), description_scrolled, 0, 3, 2, 1);
+
+	e->attendees.layout = gtk_layout_new(NULL, NULL);
+	e->attendees.cheight = 20; // TODO: base on actual widget size
+
+	g_signal_connect(G_OBJECT(e->attendees.layout), "size-allocate", G_CALLBACK(attendee_layout_relayout), &e->attendees);
+
+	gtk_box_pack_start(GTK_BOX(e->details), grid_left, TRUE, TRUE, 5);
+	gtk_box_pack_start(GTK_BOX(e->details), e->attendees.layout, TRUE, TRUE, 5);
 
 	// hack
-	//    GtkStyleContext* sc = gtk_widget_get_style_context(e->bar);
-	//    GdkRGBA col;
-	//    gtk_style_context_get_background_color(sc, GTK_STATE_FLAG_NORMAL, &col);
-	//    gtk_widget_override_background_color(e->details, GTK_STATE_FLAG_NORMAL, &col);
+	GtkStyleContext* sc = gtk_widget_get_style_context(e->bar);
+	GdkRGBA col;
+	gtk_style_context_get_background_color(sc, GTK_STATE_FLAG_NORMAL, &col);
+	gtk_widget_override_background_color(e->details, GTK_STATE_FLAG_NORMAL, &col);
 
-	g_signal_connect(GTK_WIDGET(e), "show", (GCallback) &first_show, e);
-	gtk_box_pack_start(GTK_BOX(e), e->bar, 0, 0, 0);
-	gtk_box_pack_start(GTK_BOX(e), e->details, 0, 0, 0);
+	g_signal_connect(GTK_WIDGET(e), "show", (GCallback) &first_show, NULL);
+	gtk_box_pack_start(GTK_BOX(e), e->bar, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(e), e->details, TRUE, TRUE, 0);
 	return (GtkWidget*) e;
 }
 
 void event_panel_set_event(EventPanel* ew, icalcomponent* ev)
 {
+	attendee_layout_clear(&ew->attendees);
 	if (ev) {
+		icaltimetype dt;
+		char time_buf[6];
 		gtk_label_set_label(GTK_LABEL(ew->event_label), icalcomponent_get_summary(ev));
-		gtk_label_set_label(GTK_LABEL(ew->description), icalcomponent_get_description(ev));
+
+		// TODO: timezone conversion
+		dt = icalcomponent_get_dtstart(ev);
+		snprintf(time_buf, 6, "%02d:%02d", dt.hour, dt.minute);
+		gtk_label_set_label(GTK_LABEL(ew->starts_at), time_buf);
+
+		dt = icalcomponent_get_dtend(ev);
+		snprintf(time_buf, 6, "%02d:%02d", dt.hour, dt.minute);
+		gtk_label_set_label(GTK_LABEL(ew->ends_at), time_buf);
+
+		gtk_text_buffer_set_text(GTK_TEXT_BUFFER(ew->description), icalcomponent_get_description(ev), -1);
 		for (icalproperty* attendee = icalcomponent_get_first_property(ev, ICAL_ATTENDEE_PROPERTY); attendee; attendee = icalcomponent_get_next_property(ev, ICAL_ATTENDEE_PROPERTY)) {
-			// TODO: populate attendee
-			// icalproperty_get_parameter_as_string(attendees, "CN")
-			// gtk_container_add(GTK_CONTAINER(ew->invited), gtk_label_new("test")); //icalproperty_get_parameter_as_string(attendees,"CN")));
+			attendee_layout_add(&ew->attendees, icalproperty_get_parameter_as_string(attendee, "CN"));
+		}
+		gtk_widget_show_all(ew->attendees.layout);
+		if (gtk_widget_get_realized(ew->attendees.layout)) {
+			GtkAllocation alloc;
+			gtk_widget_get_allocation(ew->attendees.layout, &alloc);
+			attendee_layout_relayout(ew->attendees.layout, &alloc, &ew->attendees);
 		}
 		gtk_widget_show(ew->bar);
 	} else {
