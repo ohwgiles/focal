20b7e72cbea69b94c1d081cb7e4afee72adecdf3
Oliver Giles <ohw.giles@gmail.com>
2018-04-21 18:56:09 +0300

    resolves #28: support ccs-calendarserver
    Greatly improve the caldav client to be more standards-compliant

--- caldav-client.c
+++ caldav-client.c
@@ -25,70 +25,144 @@ struct _CaldavClient {
 };
 
 typedef struct {
-	char* nsDav;
-	char* nsCalSrv;
-	char* nsCalDav;
-	char* tagCalendarData;
-	char* tagDisplayName;
-	char* tagCTag;
+	int depth;
+	char* name;
+} XmlNs;
+
+typedef struct {
 	GString chars;
+	int depth;
+	GQueue* ns_defaults;
+	GHashTable* ns_aliases;
+	GSList* hrefs;
 	GSList* event_list;
 } XmlParseCtx;
 
+static void xmlns_free(XmlNs* ns)
+{
+	g_free(ns->name);
+	g_free(ns);
+}
+
+// Checks the list of passed XML attributes for default namespaces
+// and namespace aliases and updates the context accordingly. Call when
+// entering a new XML tag pair.
+static void xmlparse_ns_push(XmlParseCtx* xpc, const xmlChar** atts)
+{
+	xpc->depth++;
+	if (!atts)
+		return;
+
+	// loop over attributes to find the namespace names
+	for (const xmlChar** k = atts; *k; k += 2) {
+		const xmlChar* v = k[1];
+		if (strcmp(*k, "xmlns") == 0) {
+			XmlNs* ns = g_new(XmlNs, 1);
+			ns->depth = xpc->depth;
+			ns->name = g_strdup(v);
+			g_queue_push_head(xpc->ns_defaults, ns);
+		} else if (strncmp(*k, "xmlns:", 6) == 0) {
+			g_hash_table_insert(xpc->ns_aliases, g_strdup(v), g_strdup(*k + 6));
+		}
+	}
+}
+
+// If leaving an XML tag pair, check if the current default namespace
+// scope has ended and pop it from the stack if so. Call when leaving
+// an XML tag pair.
+static void xmlparse_ns_pop(XmlParseCtx* xpc)
+{
+	XmlNs* ns = g_queue_peek_head(xpc->ns_defaults);
+	if (ns && ns->depth == xpc->depth) {
+		xmlns_free(ns);
+		g_queue_pop_head(xpc->ns_defaults);
+	}
+}
+
+// Helper function to match a found XML tag, taking all namespace aliases
+// and default namespaces into account
+static gboolean xml_tag_matches(XmlParseCtx* xpc, const char* tag, const char* expectedNs, const char* expectedTag)
+{
+	const char* nsc = strchr(tag, ':');
+	if (nsc) {
+		char* nsAbbr = g_hash_table_lookup(xpc->ns_aliases, expectedNs);
+		if (nsAbbr)
+			return nsc - tag == strlen(nsAbbr) && memcmp(nsAbbr, tag, nsc - tag) == 0 && strcmp(expectedTag, nsc + 1) == 0;
+	} else {
+		XmlNs* topns = g_queue_peek_head(xpc->ns_defaults);
+		if (topns)
+			return strcmp(topns->name, expectedNs) == 0 && strcmp(expectedTag, tag) == 0;
+	}
+	return FALSE;
+}
+
+// Callback when SAX parser ends a CDATA section. Store the content, it
+// will probably be needed (checked in the endElement callback)
 static void xmlparse_characters(void* ctx, const xmlChar* ch, int len)
 {
 	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
 	g_string_append_len(&xpc->chars, ch, len);
 }
 
-static void xmlparse_init_start(void* ctx, const xmlChar* fullname, const xmlChar** atts)
+// Callback when SAX parser finds an open XML tag (all-purpose)
+static void xmlparse_tag_open(void* ctx, const xmlChar* fullname, const xmlChar** atts)
 {
 	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
-
-	// return if we already found the namespace names
-	if (xpc->nsDav)
-		return;
-
-	if (!strstr(fullname, ":multistatus"))
-		// this is not the XML tag we're looking for
-		return;
-
-	// loop over attributes to find the namespace names
-	for (const xmlChar** k = atts; *k; k += 2) {
-		const xmlChar* v = k[1];
-		if (strncmp(*k, "xmlns:", 6) == 0) {
-			if (xpc->nsDav == NULL && strcmp(v, "DAV:") == 0) {
-				xpc->nsDav = strdup(*k + 6);
-				asprintf(&xpc->tagDisplayName, "%s:displayname", *k + 6);
-			} else if (xpc->nsCalSrv == NULL && strcmp(v, "http://calendarserver.org/ns/") == 0) {
-				xpc->nsCalSrv = strdup(*k + 6);
-				asprintf(&xpc->tagCTag, "%s:getctag", *k + 6);
-			} else if (xpc->nsCalDav == NULL && strcmp(v, "urn:ietf:params:xml:ns:caldav") == 0) {
-				xpc->nsCalDav = strdup(*k + 6);
-				asprintf(&xpc->tagCalendarData, "%s:calendar-data", *k + 6);
-			}
-		}
-	}
-}
-
-static void xmlparse_init_end(void* ctx, const xmlChar* name)
-{
-	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
+	xmlparse_ns_push(xpc, atts);
 	g_string_truncate(&xpc->chars, 0);
 }
 
-static void xmlparse_sync_end(void* ctx, const xmlChar* name)
+// Callback when SAX parser finds a closing XML tag during calendar init
+static void xmlparse_init_close(void* ctx, const xmlChar* name)
 {
 	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
-	if (xpc->tagCalendarData && strcmp(name, xpc->tagCalendarData) == 0) {
+	// TODO save ctag and displayname
+	xmlparse_ns_pop(xpc);
+}
+
+// Callback when SAX parser finds a closing XML tag during search for hrefs
+static void xmlparse_find_hrefs(void* ctx, const xmlChar* name)
+{
+	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
+	if (xml_tag_matches(xpc, name, "DAV:", "href")) {
+		xpc->hrefs = g_slist_append(xpc->hrefs, g_strdup(xpc->chars.str));
+	}
+	xmlparse_ns_pop(xpc);
+}
+
+// Callback when SAX parser finds a closing XML tag during calendar-data REPORT
+static void xmlparse_find_caldata(void* ctx, const xmlChar* name)
+{
+	XmlParseCtx* xpc = (XmlParseCtx*) ctx;
+	if (xml_tag_matches(xpc, name, "urn:ietf:params:xml:ns:caldav", "calendar-data")) {
 		icalcomponent* comp = icalparser_parse_string(xpc->chars.str);
 		icalcomponent* vevent = icalcomponent_get_first_component(comp, ICAL_VEVENT_COMPONENT);
 		if (vevent) {
-			// printf("got event %s\n", icalcomponent_get_summary(vevent));
 			xpc->event_list = g_slist_append(xpc->event_list, vevent);
 		}
 	}
-	g_string_truncate(&xpc->chars, 0);
+
+	xmlparse_ns_pop(xpc);
+}
+
+// Initializes an XmlParseCtx. Those members which are only used in specific operations
+// must be allocated manually after calling this function
+static void xmlctx_init(XmlParseCtx* ctx)
+{
+	memset(ctx, 0, sizeof(XmlParseCtx));
+	ctx->ns_defaults = g_queue_new();
+	ctx->ns_aliases = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
+	g_string_set_size(&ctx->chars, 0);
+}
+
+// Cleans up an XmlParseCtx. Any members allocated manually after xmlctx_init must also
+// be cleaned up manually
+static void xmlctx_cleanup(XmlParseCtx* ctx)
+{
+	g_queue_free_full(ctx->ns_defaults, (GDestroyNotify) xmlns_free);
+	g_hash_table_remove_all(ctx->ns_aliases);
+	g_free(ctx->chars.str);
+	g_hash_table_destroy(ctx->ns_aliases);
 }
 
 static size_t curl_write_to_gstring(char* ptr, size_t size, size_t nmemb, void* userdata)
@@ -109,6 +183,7 @@ CaldavClient* caldav_client_new(const char* url, const char* user, const char* p
 
 gboolean caldav_client_init(CaldavClient* cc)
 {
+	CURLcode ret;
 	CURL* curl = curl_easy_init();
 
 	if (!curl)
@@ -121,33 +196,39 @@ gboolean caldav_client_init(CaldavClient* cc)
 	headers = curl_slist_append(headers, "Prefer: return-minimal");
 	headers = curl_slist_append(headers, "Content-Type: application/xml; charset=utf-8");
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 	curl_easy_setopt(curl, CURLOPT_USERNAME, cc->username);
 	curl_easy_setopt(curl, CURLOPT_PASSWORD, cc->password);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, cc->verify_cert);
 
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS,
-					 "<d:propfind xmlns:d=\"DAV:\" "
-					 "xmlns:cs=\"http://calendarserver.org/ns/\">"
-					 "  <d:prop>"
-					 "     <d:displayname />"
-					 "     <cs:getctag />"
-					 "  </d:prop>"
-					 "</d:propfind>");
+					 "<propfind xmlns=\"DAV:\">"
+					 "  <prop>"
+					 "     <displayname />"
+					 "     <getctag xmlns=\"http://calendarserver.org/ns/\"/>"
+					 "  </prop>"
+					 "</propfind>");
 
-	GString* str = g_string_new(NULL);
+	GString* init_resp = g_string_new(NULL);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_to_gstring);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, str);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, init_resp);
 
-	if (curl_easy_perform(curl) != CURLE_OK)
-		return curl_easy_cleanup(curl), FALSE;
-
-	XmlParseCtx ctx = {0};
-	xmlSAXHandler my_handler = {.characters = xmlparse_characters,
-								.startElement = xmlparse_init_start,
-								.endElement = xmlparse_init_end};
-	xmlSAXUserParseMemory(&my_handler, &ctx, str->str, str->len);
-
+	ret = curl_easy_perform(curl);
 	curl_easy_cleanup(curl);
+
+	if (ret != CURLE_OK) {
+		g_string_free(init_resp, TRUE);
+		return FALSE;
+	}
+
+	XmlParseCtx ctx;
+	xmlctx_init(&ctx);
+	xmlSAXHandler my_handler = {.characters = xmlparse_characters,
+								.startElement = xmlparse_tag_open,
+								.endElement = xmlparse_init_close};
+	xmlSAXUserParseMemory(&my_handler, &ctx, init_resp->str, init_resp->len);
+	xmlctx_cleanup(&ctx);
+	g_string_free(init_resp, TRUE);
 
 	return TRUE;
 }
@@ -173,6 +254,7 @@ gboolean caldav_client_put(CaldavClient* cc, icalcomponent* event, const char* u
 	headers = curl_slist_append(headers, "Expect:");
 
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 	curl_easy_setopt(curl, CURLOPT_USERNAME, cc->username);
 	curl_easy_setopt(curl, CURLOPT_PASSWORD, cc->password);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, cc->verify_cert);
@@ -210,6 +292,7 @@ gboolean caldav_client_delete(CaldavClient* cc, icalcomponent* event, const char
 	// headers = curl_slist_append(headers, "If-Match: ETAG_HERE");
 
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 	curl_easy_setopt(curl, CURLOPT_USERNAME, cc->username);
 	curl_easy_setopt(curl, CURLOPT_PASSWORD, cc->password);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, cc->verify_cert);
@@ -224,6 +307,7 @@ gboolean caldav_client_delete(CaldavClient* cc, icalcomponent* event, const char
 
 GSList* caldav_client_sync(CaldavClient* cc)
 {
+	CURLcode ret;
 	CURL* curl = curl_easy_init();
 
 	if (!curl)
@@ -236,26 +320,71 @@ GSList* caldav_client_sync(CaldavClient* cc)
 	headers = curl_slist_append(headers, "Prefer: return-minimal");
 	headers = curl_slist_append(headers, "Content-Type: application/xml; charset=utf-8");
 	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 	curl_easy_setopt(curl, CURLOPT_USERNAME, cc->username);
 	curl_easy_setopt(curl, CURLOPT_PASSWORD, cc->password);
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, cc->verify_cert);
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "<propfind xmlns='DAV:'><prop><calendar-data xmlns='urn:ietf:params:xml:ns:caldav'/></prop></propfind>");
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "<propfind xmlns=\"DAV:\"><prop><href/></prop></propfind>");
 
-	GString* str = g_string_new(NULL);
+	GString* propfind_resp = g_string_new(NULL);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_to_gstring);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, str);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, propfind_resp);
 
-	if (curl_easy_perform(curl) != CURLE_OK)
-		return curl_easy_cleanup(curl), NULL;
+	// First, a PROPFIND request...
+	ret = curl_easy_perform(curl);
 
-	XmlParseCtx ctx = {.event_list = g_slist_alloc()};
+	if (ret != CURLE_OK) {
+		g_string_free(propfind_resp, TRUE);
+		curl_easy_cleanup(curl);
+		return NULL;
+	}
 
+	XmlParseCtx ctx;
+	xmlctx_init(&ctx);
+	// list containing all <href> URLs
+	ctx.hrefs = g_slist_alloc();
 	xmlSAXHandler my_handler = {.characters = xmlparse_characters,
-								.startElement = xmlparse_init_start,
-								.endElement = xmlparse_sync_end};
-	xmlSAXUserParseMemory(&my_handler, &ctx, str->str, str->len);
+								.startElement = xmlparse_tag_open,
+								.endElement = xmlparse_find_hrefs};
+	xmlSAXUserParseMemory(&my_handler, &ctx, propfind_resp->str, propfind_resp->len);
+	xmlctx_cleanup(&ctx);
+	g_string_free(propfind_resp, TRUE);
 
+	GString* report_req = g_string_new(
+		"<c:calendar-multiget xmlns:d=\"DAV:\" xmlns:c=\"urn:ietf:params:xml:ns:caldav\">"
+		"  <d:prop>"
+		"    <d:getetag/>"
+		"    <c:calendar-data/>"
+		"  </d:prop>");
+	for (GSList* p = ctx.hrefs; p; p = p->next) {
+		g_string_append_printf(report_req, "<d:href>%s</d:href>", (char*) p->data);
+	}
+	g_string_append(report_req, "</c:calendar-multiget>");
+	g_slist_free_full(ctx.hrefs, g_free);
+
+	// ... and then REPORT, reusing the same CURL*
+	GString* report_resp = g_string_new(NULL);
+	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "REPORT");
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, report_req->str);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, report_resp);
+
+	ret = curl_easy_perform(curl);
 	curl_easy_cleanup(curl);
+	g_string_free(report_req, TRUE);
+
+	if (ret != CURLE_OK) {
+		g_string_free(report_resp, TRUE);
+		return NULL;
+	}
+
+	xmlctx_init(&ctx);
+	ctx.event_list = g_slist_alloc();
+	xmlSAXHandler my_handler2 = {.characters = xmlparse_characters,
+								 .startElement = xmlparse_tag_open,
+								 .endElement = xmlparse_find_caldata};
+	xmlSAXUserParseMemory(&my_handler2, &ctx, report_resp->str, report_resp->len);
+	xmlctx_cleanup(&ctx);
+	g_string_free(report_resp, TRUE);
 
 	return ctx.event_list;
 }
